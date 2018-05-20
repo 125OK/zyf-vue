96dd1d6ff322da522010622d55a265b3b6604dc7
zyf <1256886029@qq.com>
2018-05-20 23:45:19 +0800

    1 commit

---
+++ sh/js/base.js
@@ -0,0 +1,3 @@
+var open=true;
+
+export {open};
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports={
+    entry:{
+        home:'./sh/js/home.js'
+    },
+    output:{
+        filename:"[name].bundle.js",
+        path: __dirname+"/dist"
+    }
+
+}
